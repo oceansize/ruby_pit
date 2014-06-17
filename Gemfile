10e52145c233578ccb9d5752572244b686f2495a
Roi Driscoll <roi.driscoll@gmail.com>
2014-06-17 23:58:33 +0100

    Create database & first model

--- Gemfile
+++ Gemfile
@@ -1,10 +1,15 @@
 # define our source to loook for gems
-source "http://rubygems.org/"
+source "https://rubygems.org"
 
 # declare the sinatra dependency
 gem "sinatra"
+gem 'data_mapper'
+gem 'dm-postgres-adapter'
+gem 'database_cleaner'
 
 # setup our test group and require rspec
 group :test do
   gem "rspec"
 end
+
+# To talk to the database, we'll need the datamapper gem
---
+++ spec/link_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe Link do
+  context 'Demonstration of how DataMapper works' do
+    it "should be created and then retrieved from the db" do
+      # in the beginning the database is empty, so should have no links:
+      expect(Link.count).to eq (0)
+      # This creates the database, so it's stored on the disk:
+      Link.create(:title => "Makers Academy",
+                  :url   => "http://www.makersacademy.com/")
+      # We ask the database how many links we have, it should be 1
+      expect(Link.count).to eq(1)
+      # Let's get the first (and only) link from the database:
+      link = Link.first
+      # Now it has all properties that it was saved with.
+      expect(link.url).to eq "http://www.makersacademy.com/"
+      expect(link.title).to eq("Makers Academy")
+      # If we want to, we can destroy it
+      link.destroy
+      # so now we have no links in the database
+      expect(Link.count).to eq 0
+    end
+  end
+end
